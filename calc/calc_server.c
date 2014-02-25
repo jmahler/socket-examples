23d34d91c396241b10690e8585b5ebc0022f4e23
Jeremiah Mahler <jmmahler@gmail.com>
2014-02-24 19:03:42 -0800

    start of calc_server simplification, removed debug
    calc_server.c was provided as part of a intro to
    networking class (EECE 555) at CSU Chico.
    It has lots of features that are unnecessary such as
    threading, support for multiple simultaneous connections,
    and detailed debugging output.
    
    This first commit removes all of the debug output.

--- calc/calc_server.c
+++ calc/calc_server.c
@@ -21,23 +21,19 @@ static void *handle_connection(void *);
 struct thread_arg_t {
 	pthread_t thread_id;
 	int socket_fd;			// Socket for thread to use
-	char debug;				// Set if thread should print debug info
 	char done;				// Set when thread copies data
 };
 
 int main(int argc, char *argv[]) {
-	if( (argc != 2) && !((argc == 3) && !strncmp(argv[2], "-d", 3)) ) {
-		fprintf(stderr, "Usage: %s <listen port> [-d]\n", argv[0]);
+
+	if(argc != 2) {
+		fprintf(stderr, "Usage: %s <listen port>\n", argv[0]);
 		return -1;
-	}
-	char debug = 0;
-	if( argc == 3 ){
-		debug = 1;
 	}
 
 	/* Get the address structures to use */
 	struct addrinfo hints;
-	struct addrinfo *results, *rp;
+	struct addrinfo *results;
 	int res = 0;
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;
@@ -46,37 +42,6 @@ int main(int argc, char *argv[]) {
 	if( (res = getaddrinfo(NULL, argv[1], &hints, &results)) != 0 ){
 		fprintf(stderr, "%s\n", gai_strerror(res));
 		return -1;
-	}
-
-	/* Print all the addresses in debug */
-	if( debug ){
-		struct protoent *proto = NULL;
-		struct sockaddr_in *ip = NULL;
-		for( rp = results; debug && (rp != NULL); rp = rp->ai_next ){
-			switch( rp->ai_family ){
-				case AF_INET:
-					printf("Family: AF_INET\n");
-					char buff[INET_ADDRSTRLEN];
-					ip = (struct sockaddr_in*)(rp->ai_addr);
-					if( !inet_ntop(ip->sin_family, &(ip->sin_addr), buff, INET_ADDRSTRLEN) ){
-						perror("inet_ntop");
-						return -1;
-					}
-					printf("\tAddress: %s\n", buff);
-					printf("\tPort: %d\n", ntohs(ip->sin_port));
-					break;
-				default: printf("Family: UNEXPECTED\n"); break;
-			}
-			proto = getprotobynumber(rp->ai_protocol);
-			printf("\tProtocol: ");
-			if( proto ){
-				printf("%s\n", proto->p_name);
-			}
-			else{
-				printf("ERROR\n");
-			}
-		}
-		endprotoent();
 	}
 
 	/* Check we only have one address */
@@ -91,9 +56,6 @@ int main(int argc, char *argv[]) {
 		perror("socket");
 		return -1;
 	}
-	if( debug ){
-		printf("Created socket with fd=%d\n", socket_fd);
-	}
 
 	/* Bind the socket */
 	if( bind(socket_fd, results->ai_addr, results->ai_addrlen) < 0 ){
@@ -102,55 +64,27 @@ int main(int argc, char *argv[]) {
 	}
 	freeaddrinfo(results);
 	results = 0;
-	if( debug ) {
-		printf("Socket named successfully\n");
-	}
 
 	/* Listen to the socket */
 	if( listen(socket_fd, 10) < 0 ){
 		perror("listen");
 		return -1;
 	}
-	if( debug ){
-		printf("Listening on socket...\n");
-	}
 
 	/* Accept and dispatch connections forever */
 	while(1) {
 		/* Accept a new connection */
-		struct sockaddr_in peer;
-		socklen_t peer_len = sizeof(struct sockaddr_in);
 		int new_conn;
-		if( debug ){
-			new_conn = accept(socket_fd, (struct sockaddr *)(&peer), &peer_len);
-		}
-		else{
-			new_conn = accept(socket_fd, NULL, NULL);
-		}
+		new_conn = accept(socket_fd, NULL, NULL);
 		if( new_conn < 0 ){
 			perror("accept");
 			if( close(socket_fd) <0 ){perror("close");}
 			return -1;
 		}
-		if( debug && (peer.sin_family != AF_INET) ){
-			fprintf(stderr, "Connection made to incompatible family");
-			if( close(socket_fd) <0 ){perror("close");}
-			return -1;
-		}
-
-		if( debug ){
-			char buff[INET_ADDRSTRLEN];
-			if( !inet_ntop(peer.sin_family, &(peer.sin_addr), buff, INET_ADDRSTRLEN) ){
-				perror("inet_ntop");
-				return -1;
-			}
-			printf("Connected to %s[%d] on fd=%d\n", buff, ntohs(peer.sin_port), new_conn);
-		}
 
 		/* Spawn a new thread for the connection */
 		struct thread_arg_t thread_arg;
 		thread_arg.done = 0;
-		thread_arg.debug = debug;
 		thread_arg.socket_fd = new_conn;
 		pthread_attr_t thread_attr;
 		if( (errno = pthread_attr_init(&thread_attr)) ){
@@ -167,9 +101,6 @@ int main(int argc, char *argv[]) {
 			perror("pthread_create");
 			if( close(socket_fd) <0 ){perror("close");}
 			return -1;
-		}
-		if( debug ){
-			printf("Spawned thread %x for fd=%d\n", (int)thread_arg.thread_id, new_conn);
 		}
 		if( (errno = pthread_attr_destroy(&thread_attr)) ){
 			perror("pthread_attr_destroy");
@@ -191,13 +122,8 @@ static void *handle_connection(void *arg) {
 	struct thread_arg_t *args_p = (struct thread_arg_t*)arg;
 	pthread_t thread_id = args_p->thread_id;
 	int socket_fd = args_p->socket_fd;
-	char debug = args_p->debug;
 	args_p->done = 1;
 	args_p = NULL;
-
-	if( debug ){
-		printf("%x: started with fd=%d\n", (int)thread_id, socket_fd);
-	}
 
 	/* Receive the next expression */
 	char expression[MAX_EXPRESSION_LEN];
@@ -220,14 +146,8 @@ static void *handle_connection(void *arg) {
 				perror("close"); exit(-1);
 			}
 			done = 1;
-			if( debug ){
-				printf("%x: Socket closed, ending thread...\n", (int)thread_id);
-			}
 		}
 		else{
-			if( debug ){
-				printf("%x: Received expression \'%s\'\n", (int)thread_id, expression);
-			}
 			float a, b, answer;
 			char op;
 			int ret = sscanf(expression, "%g%c%g", &a, &op, &b);
@@ -235,9 +155,6 @@ static void *handle_connection(void *arg) {
 				answer = NAN;
 			}
 			else{
-				if( debug ){
-					printf("%x: Parsed expression: %f %c %f\n", (int)thread_id, a, op, b);
-				}
 				switch(op) {
 					case '*': answer = a * b; break;
 					case '/': answer = a / b; break;
@@ -248,9 +165,6 @@ static void *handle_connection(void *arg) {
 				}
 			}
 			sprintf(expression, "%g", answer);
-			if( debug ){
-				printf("%x: Sending answer \'%s\'\n", (int)thread_id, expression);
-			}
 			ssize_t exp_len = strlen(expression) + 1, len, sent_len = 0;
 			if( exp_len > MAX_RESPONSE_LEN ){
 				len = exp_len;
